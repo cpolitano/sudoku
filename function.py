5bba1da9f4e42b83d5a116d7bf3e3233a677044f
Clare Politano Hutchings <cpolitano8@gmail.com>
2018-01-30 20:36:14 -0600

    assign board values

--- function.py
+++ function.py
@@ -6,15 +6,22 @@ cols = '123456789'
 def cross(a, b):
     return [s+t for s in a for t in b]
 
-boxes = cross(rows, cols)
+def create_board():
+    boxes = cross(rows, cols)
+    row_units = [cross(r, cols) for r in rows]
+    column_units = [cross(rows, c) for c in cols]
+    square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+    unitlist = row_units + column_units + square_units
+    return unitlist
 
-row_units = [cross(r, cols) for r in rows]
-print(row_units)
-
-column_units = [cross(rows, c) for c in cols]
-print(column_units)
-
-square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-print(square_units)
-
-unitlist = row_units + column_units + square_units
+def grid_values(board_values):
+    values = {}
+    board = create_board()
+    if len(board) == len(board_values):
+        counter = 0
+        for b in board:
+            values[b] = board_values[counter]
+            counter++
+    else:
+        raise ValueError('invalid board values. pls do better')
+    return values
