63fec6ab09df88618c3e917447255649c48c2fe5
Clare Politano Hutchings <cpolitano8@gmail.com>
2018-02-02 20:03:39 -0600

    begin to implement search

--- function.py
+++ function.py
@@ -2,6 +2,8 @@
 from utils import *
 
 test_string = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
+another_string = '..3.2.6..9..3.5..1...8..4....81.29..7.......8..6..82.....6.95..8..2.3..9..5.1.3..'
+harder_string = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
 all_digits = '123456789'
 
 def assign_grid(board_values):
@@ -28,10 +30,9 @@ def eliminate(values):
 
 # print(eliminate(assign_grid(test_string)))
 
-def only_values(values):
+def only_choice(values):
     """iterate over boxes, and assign any values that are only possible for a box"""
     for unit in unitlist:
-        digits = {}
         for digit in all_digits:
             digit_boxes = [];
             for box in unit:
@@ -41,6 +42,34 @@ def only_values(values):
                 values[digit_boxes[0]] = digit
     return values
 
-grid = assign_grid(test_string)
-eliminated = eliminate(grid)
-print(only_values(eliminated))
+def count_boxes(values, desired_count):
+    """boxes with specified available values remaining"""
+    boxes_with_value_count = [box for box in values.keys() if len(values[box]) == desired_count]
+    return len(boxes_with_value_count)
+
+def reduce_puzzle(values):
+    stalled_or_solved = False
+    while not stalled_or_solved:
+        print('reducing puzzle...')
+        solved_values_before = count_boxes(values, 1)
+        eliminate(values)
+        only_choice(values)
+        solved_values_after = count_boxes(values, 1)
+        stalled_or_solved = solved_values_before == solved_values_after | solved_values_after == 81
+        if count_boxes(values, 0) > 0:
+            return False
+    return values
+
+def search(values):
+    values = reduce_puzzle(values)
+    if values == False:
+        return False
+    if all(box for box in values if len(values[box]) == 1):
+        return values # if all boxes have 1 value, puzzle is solved
+    unsolved_boxes = [box for box in values if len(values[box]) > 1]
+    print(unsolved_boxes)
+    # closest_unsolved_box = min([box for ])
+
+
+grid = assign_grid(another_string)
+search(grid)
